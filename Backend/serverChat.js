3d901263dde0a6c37390499ffdeb1c51cdf4674c
Esstargirly <estherappolos51@gmail.com>
2026-01-13 23:12:47 +0100

    update .gitignore and increase JSON size limit in serverChat.js

--- Backend/serverChat.js
+++ Backend/serverChat.js
@@ -7,7 +7,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+
+//increase size limit
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({limit: "10mb", extended: true}));
 
 const API_URL = "https://api.mistral.ai/v1/chat/completions";
 
